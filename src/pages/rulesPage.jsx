f0ae35ed1c6885620db7050128230683b91336a8
CynthiaVerde05 <21090572@alumno.utmetropolitana.edu.mx>
2024-07-01 22:37:00 -0600

    change style in rulesPage

--- src/pages/rulesPage.jsx
+++ src/pages/rulesPage.jsx
@@ -13,11 +13,11 @@ export default function RulesPage() {
           </div>
           <hr />
           <div className="text-rules">
-            <h2>1. Solo tendrás 15 segundos para responder cada pregunta..</h2>
-            <h2>2. Una vez que seleccionas tu respuesta, no se puede cambiar</h2>
-            <h2>3. No puedes seleccionar ninguna opción una vez acabado el tiempo..</h2>
-            <h2>4. No puedes salir del examen hasta que termines el tiempo</h2>
-            <h2>5. Obtendrás tu resultado al final del examen.</h2>
+            <span>1. Solo tendrás 15 segundos para responder cada pregunta.</span><br/>
+            <span>2. Una vez que seleccionas tu respuesta, no se puede cambiar.</span><br/>
+            <span>3. No puedes seleccionar ninguna opción una vez acabado el tiempo.</span><br/>
+            <span>4. No puedes salir del examen hasta que termines el tiempo.</span><br/>
+            <span>5. Obtendrás tu resultado al final del examen.</span>
           </div>
           <button className="start-button" onClick={handleClick}>Iniciar Examen</button>
         </div>
